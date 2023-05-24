fe4e34de5683b6b4f4ece7f63b61483cb69cbcbd
bmessaoudi <bilelmessaudi@gmail.com>
2023-05-24 16:08:45 +0200

    comment others page path & add my story page

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ export default class App extends Component {
           <Navbar />
           <Routes>
             <Route path='/' exact element={<Home/>} />
-            <Route path='/services' element={<Services/>} />
+            {/* <Route path='/services' element={<Services/>} />
             <Route path='/products' exact element={<Products/>} />
-            <Route path='/sign-up' exact element={<SignUp/>} />
+            <Route path='/sign-up' exact element={<SignUp/>} /> */}
           </Routes>
         </Router>    
       </>
---
+++ src/components/MyStory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyStory() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyStory
